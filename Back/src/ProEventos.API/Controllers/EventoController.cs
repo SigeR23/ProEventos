49a5d344510d5ed0f72b076b22aa0512a809fdea
Reginaldo Lessa Ferreira da Silva <Reginaldo.Silva@targetsistemas.com.br>
2022-09-12 18:01:58 -0300

    Seção 2

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _eventos = new Evento[] {
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "São Paulo",
+                Lote = "Primeiro Lote",
+                QtdPessoas = 50,
+                DataEvento = DateTime.Now.AddDays(15).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+            },
+            new Evento() {
+                EventoId = 2,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Rio de Janeiro",
+                Lote = "Primeiro Lote",
+                QtdPessoas = 50,
+                DataEvento = DateTime.Now.AddDays(30).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+            }
+        };
+
+        public EventoController() { }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _eventos;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _eventos.Where(e => e.EventoId == id);
+        }
+    }
+}
