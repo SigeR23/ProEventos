d4d2f9dbf5a57a1a138b0d92660ddd127ea675a0
Reginaldo Lessa Ferreira da Silva <Reginaldo.Silva@targetsistemas.com.br>
2022-12-07 13:36:42 -0300

    Seção 5: Angular - Interpolação, Diretivas e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
-
+//ANGULAR
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core';
+
+//BOOTSTRAP
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+//COMPONENTS
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './Nav/Nav.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-      PalestrantesComponent,
-      PalestrantesComponent
+    NavComponent,
+    PalestrantesComponent
+
    ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    CollapseModule.forRoot(),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  public widthImg = 50;
+  public marginImg = 2;
+  public imgIsCollapsed = false;
+  private _filtro = '';
+
+  public get filtro() : string {
+    return this._filtro;
+  }
+
+  public set filtro(value: string) {
+    this._filtro = value;
+    this.eventosFiltrados = this._filtro ? this.filtrarEventos(this._filtro) : this.eventos;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +30,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  filtrarEventos(filtrarPor: string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter((e: { tema: string; local: string; }) => e.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || e.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
+  }
+
   public getEventos(): void {
     this.http.get("https://localhost:5001/api/Eventos").subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error)
     );
   }
